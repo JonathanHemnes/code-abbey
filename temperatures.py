bd127cea252736fe708d3503d074a506ae3d883d
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-03-05 14:23:18 -0700

    cleanup

--- temperatures.py
+++ temperatures.py
@@ -3,9 +3,7 @@ def getTemp(f):
     c = ((f + 40) / 1.8) - 40
     return round(c)
 
-values = input().split()
-
-values = values[1:]
+values = input().split()[1:]
 
 values = [int(i) for i in values]
 
