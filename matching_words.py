4ffd438d34053fd589197460b1351a60a72db0d8
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-03-19 19:30:18 -0600

    matching words

---
+++ matching_words.py
@@ -0,0 +1,13 @@
+words = input().split()
+
+wordset = {}
+
+answers = set()
+
+for w in words:
+    try:
+        wordset[w] = wordset[w] + 1
+        answers.add(w)
+    except KeyError:
+        wordset[w] = 1
+print(' '.join(sorted(answers)))
